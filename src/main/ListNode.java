e13bc96e0dcba64972f9bcba39c4e362965e92e6
Omar Ahmed <44481508+aamosa@users.noreply.github.com>
2019-04-13 09:51:09 -0500

    Create ListNode.java

---
+++ src/main/ListNode.java
@@ -0,0 +1,73 @@
+/**
+ * Basic implementation of a singly linked list.
+ *
+ */
+ public class ListNode {
+    int val;
+    ListNode next;
+    
+    // default constructor -------------------------------------------------
+    public ListNode() {}
+
+    public ListNode(int d) {
+        val = d;
+        next = null;
+    }
+    
+    // private constructor ---------------------------------------------------
+    private ListNode(ListNode node) {
+        ListNode t = this;
+        while (t.next != null) {
+            t = t.next;
+        }
+        t.next = node;
+    }
+
+    public void add(int val) {
+        if (this.val == 0 && this.next == null) {
+            this.val = val;
+        } else {
+            ListNode tmp = new ListNode(this);
+            this.val = val;
+            this.next = tmp;
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        String s = "";
+        for (ListNode p = this; p != null; p = p.next) {
+            s += String.valueOf(p.val);
+            if (p.next != null)
+                s += "->";
+        }
+        return s;
+    }
+
+    /**
+     * This helper method takes a given array and returns a singly linked list.
+     */
+    public static ListNode ArrayToLinkedList(int[] nums) {
+        ListNode node = new ListNode();
+        ListNode t = node;
+        for (int i = 0; i < nums.length; i++) {
+            t.next = new ListNode(nums[i]);
+            t = t.next;
+        }
+        return node.next;
+    }
+
+    /**
+     * This helper method prints the nodes of a linked list each per separate line.
+     */
+    public static void printLinkedList(ListNode head) {
+        if (head == null) return;
+        ListNode cur = head.next;
+        while (cur != null) {
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+    }
+
+}
